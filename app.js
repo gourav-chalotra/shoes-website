09672bb1b94114abe6ceb97d096fb1d638ac83aa
gourav-chalotra <chalgourav82@gmail.com>
2024-07-23 21:08:32 +0530

    version 1.0

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const path = require("path");
+const app = express();
+
+
+app.get("/",(req,res)=>{
+    
+})
+
+
+app.listen(3000,()=>{
+    console.log("working");
+});
